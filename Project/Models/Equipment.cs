a36ce32a7d8486b932f551b3e13cf56bbca9da1e
Dragoș Șerban <dragos4002@gmail.com>
2025-03-19 10:45:51 +0200

    Added Equipment class to Project.Models namespace

---
+++ Project/Models/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Equipment
+    {
+        public Guid EquipmentID { get; set; }
+        public string Type { get; set; }
+        public string Specification { get; set; }
+        public int Stock { get; set; }
+        public Equipment(Guid equipmentID, string type, string specification, int stock)
+        {
+            EquipmentID = equipmentID;
+            Type = type;
+            Specification = specification;
+            Stock = stock;
+        }
+    }
+}
