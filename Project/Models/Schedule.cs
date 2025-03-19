0aecec15fcd39194a8c0f349c749e39c890e1c78
Dragoș Șerban <dragos4002@gmail.com>
2025-03-19 11:22:18 +0200

    Added Schedule class to Project.Models namespace.

---
+++ Project/Models/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Schedule
+    {
+        public Guid ScheduleID { get; set; }
+        public Guid DoctorID { get; set; }
+        public Guid ShiftID { get; set; }
+        public Schedule(Guid scheduleID, Guid doctorID, Guid shiftID)
+        {
+            ScheduleID = scheduleID;
+            DoctorID = doctorID;
+            ShiftID = shiftID;
+        }
+    }
+}
