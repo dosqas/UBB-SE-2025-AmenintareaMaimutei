a4dd870fee2d116ed2c050a0e78c051c76a70c85
Dragoș Șerban <dragos4002@gmail.com>
2025-03-19 10:45:36 +0200

    Added Drug class to Project.Models namespace

---
+++ Project/Models/Drug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Drug
+    {
+        public Guid DrugID { get; set; }
+        public string Name { get; set; }
+        public string Administration { get; set; }
+        public string Specification { get; set; }
+        public int Supply { get; set; }
+
+        public Drug(Guid drugID, string name, string administration, string specification, int supply)
+        {
+            DrugID = drugID;
+            Name = name;
+            Administration = administration;
+            Specification = specification;
+            Supply = supply;
+        }
+
+    }
+}
