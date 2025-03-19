e749519d4b38d4aff547fbf3171e8fdc07538633
Dragoș Șerban <dragos4002@gmail.com>
2025-03-19 10:24:44 +0200

    Added Admin class to Project.Models namespace

---
+++ Project/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Admin
+    {
+        public Guid AdminID { get; set; }
+        public Guid UserID { get; set; }
+
+        public Admin(Guid adminId, Guid userId)
+        {
+            AdminID = adminId;
+            UserID = userId;
+        }
+    }
+}
