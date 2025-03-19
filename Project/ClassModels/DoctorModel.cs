dcaa37319d579e7610c8397a025cf2e3349a4212
Dragoș Șerban <dragos4002@gmail.com>
2025-03-19 18:35:37 +0200

    Added DoctorModel class for retrieving doctor records
    Introduces the `DoctorModel` class in the `Project.ClassModels` namespace. This class includes a method `GetDoctors` that connects to the database, executes a query to fetch all records from the `Doctors` table, and constructs a list of `Doctor` objects from the results.

---
+++ Project/ClassModels/DoctorModel.cs
@@ -0,0 +1,39 @@
+﻿using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Project.ClassModels
+{
+    public class DoctorModel
+    {
+        private readonly string _connectionString = "DE ADAUGAT CONNECTION STRING";
+
+        public List<Doctor> GetDoctors()
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT * FROM Doctors";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    doctors.Add(new Doctor(
+                        reader.GetGuid(0),
+                        reader.GetGuid(1),
+                        reader.GetGuid(2),
+                        reader.GetFloat(3),
+                        reader.GetFloat(4),
+                        reader.GetString(5)
+                    ));
+                }
+            }
+            return doctors;
+        }
+    }
+}
