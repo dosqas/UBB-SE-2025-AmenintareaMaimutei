a40bd5728a7b1de9ffb4d94d2dc935fe168efc35
dosqas <137123562+dosqas@users.noreply.github.com>
2025-03-20 17:00:02 +0200

    Added the EquipmentViewModel functionality and also the EquipmentModel AddEquipment function

--- Project/Models/Equipment.cs
+++ Project/Models/Equipment.cs
@@ -9,12 +9,17 @@ namespace Project.Models
     public class Equipment
     {
         public Guid EquipmentID { get; set; }
+
+        public string Name { get; set; }
         public string Type { get; set; }
         public string Specification { get; set; }
         public int Stock { get; set; }
-        public Equipment(Guid equipmentID, string type, string specification, int stock)
+
+        public Equipment() {}
+        public Equipment(Guid equipmentID, string name, string type, string specification, int stock)
         {
             EquipmentID = equipmentID;
+            Name = name;
             Type = type;
             Specification = specification;
             Stock = stock;
