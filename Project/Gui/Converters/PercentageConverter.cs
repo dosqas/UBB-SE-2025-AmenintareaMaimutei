fc0330c369c5baba55eb4cac57874f091eed10a2
Suba Dániel <danisuba10@gmail.com>
2025-03-20 23:24:42 +0200

    Create Consultation review page, PercentageConverter for GUI, ReviewModel class-model (#164)
    * Add .gitignore in root
    
    * Create PercentageConverter for GUI
    
    * Create ConsultationReviewPage
    
    * Consultation-review csproj changes
    
    * Create ReviewModel with Add, Remove and fetch review for MedicalRecord by ID
    
    * Add DB integration to submit button
    
    * Disable submit button on success.

---
+++ Project/Gui/Converters/PercentageConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Gui.Converters
+{
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is double width && parameter is string percentageString)
+            {
+                if (double.TryParse(percentageString, out double percentage))
+                {
+                    return width * percentage;
+                }
+            }
+            return value; // Fallback to original value
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
