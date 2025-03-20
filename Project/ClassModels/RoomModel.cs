01fa9cf0a018b4aabbc1489bcbeeb423303eaf9d
dosqas <137123562+dosqas@users.noreply.github.com>
2025-03-20 17:22:27 +0200

    Added the RoomViewModel functionality and also the RoomModel AddRoom function

--- Project/ClassModels/RoomModel.cs
+++ Project/ClassModels/RoomModel.cs
@@ -13,6 +13,37 @@ namespace Project.ClassModels
     {
         private readonly string _connectionString = DatabaseHelper.GetConnectionString();
 
+        public bool AddRoom(Room room)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Rooms (RoomID, Capacity, DepartmentID, EquipmentID) VALUES (@RoomID, @Capacity, @DepartmentID, @EquipmentID)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@RoomID", room.RoomID);
+                command.Parameters.AddWithValue("@Capacity", room.Capacity);
+                command.Parameters.AddWithValue("@DepartmentID", room.DepartmentID);
+                command.Parameters.AddWithValue("@EquipmentID", room.EquipmentID);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
+        public bool DeleteRoom(Guid roomID)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM Rooms WHERE RoomID = @RoomID";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@RoomID", roomID);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+
         public bool DoesRoomExist(Guid roomID)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
@@ -26,17 +57,32 @@ namespace Project.ClassModels
                 return count > 0;
             }
         }
-        public bool DeleteRoom(Guid roomID)
+
+        public bool DoesEquipmentExist(Guid equipmentID)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                string query = "DELETE FROM Rooms WHERE RoomID = @RoomID";
+                string query = "SELECT COUNT(*) FROM Equipments WHERE EquipmentID = @EquipmentID";
                 SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@RoomID", roomID);
+                command.Parameters.AddWithValue("@EquipmentID", equipmentID);
 
                 connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+        public bool DoesDepartmentExist(Guid departmentID)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Departments WHERE DepartmentID = @DepartmentID";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@DepartmentID", departmentID);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
             }
         }
     }
