df42ceb8d4e4aec60ee0a2d8290068dade326050
Dragoș Șerban <dragos4002@gmail.com>
2025-03-24 18:35:21 +0200

    Add UserModel class with role-checking method

---
+++ project/ClassModels/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using Project.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ClassModels
+{
+    class UserModel
+    {
+        private readonly string _connectionString = DatabaseHelper.GetConnectionString();
+
+        public bool UserExistsWithRole(int userID, string role)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE UserID = @UserID AND Role = @Role";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@UserID", userID);
+                command.Parameters.AddWithValue("@Role", role);
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+        }
+    }
+}
