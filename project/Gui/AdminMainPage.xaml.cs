198309d9ca84bd0f53129623bfed48cd7a8f386d
Dragoș Șerban <dragos4002@gmail.com>
2025-03-24 21:35:41 +0200

    Added Navigation Menu for Modify Page (#191)
    * Refactor XAML layouts for improved scrolling
    
    * Add ModifyPage for enhanced navigation functionality
    
    * Update navigation for Modify action in AdminMainPage
    
    * Update Project.csproj for ModifyPage.xaml handling

--- project/Gui/AdminMainPage.xaml.cs
+++ project/Gui/AdminMainPage.xaml.cs
@@ -56,7 +56,7 @@ namespace Project.Gui
                         ContentFrame.Navigate(typeof(AddPage));
                         break;
                     case "Modify":
-                        ContentFrame.Navigate(typeof(ModifyRoomView));
+                        ContentFrame.Navigate(typeof(ModifyPage));
                         break;
                     case "LogOut":
                         var loginPage = new LoginPage();
