24600ea343f4d04ca280f473701a2e4f0cc67dd8
saba-calin <sabaila.calin10@gmail.com>
2025-04-07 13:01:25 -0700

    added documentation for the room and review in model

--- project/Models/Review.cs
+++ project/Models/Review.cs
@@ -1,24 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project.Models
+﻿namespace Project.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a review associated with a medical record.
+    /// </summary>
     public class Review
     {
-        public int ReviewID { get; set; }
-        public int MedicalRecordID { get; set; }
-        public string Text { get; set; }
-        public int NrStars { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Review"/> class.
+        /// </summary>
+        /// <param name="reviewID">The unique identifier of the review.</param>
+        /// <param name="medicalRecordID">The ID of the associated medical record.</param>
+        /// <param name="text">The text content of the review.</param>
+        /// <param name="nrStars">The number of stars given in the review.</param>
         public Review(int reviewID, int medicalRecordID, string text, int nrStars)
         {
-            ReviewID = reviewID;
-            MedicalRecordID = medicalRecordID;
-            Text = text;
-            NrStars = nrStars;
+            this.ReviewID = reviewID;
+            this.MedicalRecordID = medicalRecordID;
+            this.Text = text;
+            this.NrStars = nrStars;
         }
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the review.
+        /// </summary>
+        public int ReviewID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the associated medical record.
+        /// </summary>
+        public int MedicalRecordID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text content of the review.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of stars given in the review.
+        /// </summary>
+        public int NrStars { get; set; }
     }
 }
