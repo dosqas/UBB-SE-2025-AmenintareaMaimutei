8f96b960a637c4c03b5c94c967f824af8c13ecc1
Silea Horia <horia.silea@gmail.com>
2025-04-08 15:52:32 +0300

    done drugs

--- project/Gui/Drugs.xaml.cs
+++ project/Gui/Drugs.xaml.cs
@@ -1,43 +1,37 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Project.Models;
-using System.Collections.ObjectModel;
-using Project.ClassModels;
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Project.Gui
 {
+    using System.Collections.ObjectModel;
+    using Microsoft.UI.Xaml.Controls;
+    using Project.ClassModels;
+    using Project.Models;
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class Drugs : Page
     {
-        public ObservableCollection<Drug> DrugsList { get; set; } = new();
-        private readonly DrugModel _drugModel = new();
+        private readonly DrugModel drugModel = new ();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Drugs"/> class.
+        /// </summary>
         public Drugs()
         {
             this.InitializeComponent();
-            Load();
+            this.Load();
         }
+
+        /// <summary>
+        /// Gets or Sets the DrugList.
+        /// </summary>
+        public ObservableCollection<Drug> DrugsList { get; set; } = new ();
+
         private void Load()
         {
-            DrugsList.Clear();
-            foreach (Drug drug in _drugModel.GetDrugs())
+            this.DrugsList.Clear();
+            foreach (Drug drug in this.drugModel.GetDrugs())
             {
-                DrugsList.Add(drug);
+                this.DrugsList.Add(drug);
             }
         }
     }
