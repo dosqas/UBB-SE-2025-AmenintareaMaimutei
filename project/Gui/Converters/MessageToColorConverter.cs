d6f1f69eaef497fbade0daf771017439467f62c1
Silea Horia <horia.silea@gmail.com>
2025-04-09 10:32:03 +0300

    done messagetocolorconverter

--- project/Gui/Converters/MessageToColorConverter.cs
+++ project/Gui/Converters/MessageToColorConverter.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Globalization;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Media;
-
 namespace Project.Utils
 {
+    using System;
+    using Microsoft.UI.Xaml.Data;
+
+    /// <summary>
+    /// A color converter, based on the message.
+    /// </summary>
     public class MessageToColorConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts the message to a corresponding color. 
+        /// </summary>
+        /// <param name="value">The message.</param>
+        /// <param name="targetType">The type.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns>The color corresponding to the message.</returns>
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is string message)
@@ -20,9 +29,18 @@ namespace Project.Utils
                     return "Red";
                 }
             }
+
             return "Red";
         }
 
+        /// <summary>
+        /// Not implemented.
+        /// </summary>
+        /// <param name="value">The color.</param>
+        /// <param name="targetType">The type.</param>
+        /// <param name="parameter">The parameter.</param>
+        /// <param name="language">The language.</param>
+        /// <returns>The message corresponding to the color.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
