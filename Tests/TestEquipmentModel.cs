40a240a1d75e55f61ebcc858ea7c50e8488ac747
Sala Darius-George <saladarius72@gmail.com>
2025-04-10 19:02:22 +0300

    tests are possible now

--- Tests/TestEquipmentModel.cs
+++ Tests/TestEquipmentModel.cs
@@ -2,6 +2,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Project.ClassModels;
 using FluentAssertions;
 using Project.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using Project.Utils;
+using System.Transactions;
 
 namespace Tests;
 
@@ -19,48 +23,99 @@ public class TestEquipmentModel
     [TestMethod]
     public void AddEquipment_ShouldReturnTrue_WhenInsertSucceeds()
     {
-        var equipment = new Equipment { Name = "Test", Type = "Type", Specification = "Spec", Stock = 5 };
-        var result = _equipmentModel.AddEquipment(equipment);
-        result.Should().BeTrue();
+        using (var scope = new TransactionScope())
+        {
+            var equipment = new Equipment { Name = "Test", Type = "Type", Specification = "Spec", Stock = 5 };
+            var result = _equipmentModel.AddEquipment(equipment);
+            result.Should().BeTrue();
+        }
+
     }
 
     [TestMethod]
     public void UpdateEquipment_ShouldReturnTrue_WhenUpdateSucceeds()
     {
-        var equipment = new Equipment { EquipmentID = 11, Name = "Test", Type = "Type", Specification = "Spec", Stock = 5 };
-        var result = _equipmentModel.UpdateEquipment(equipment);
-        result.Should().BeTrue();
+        using (var scope = new TransactionScope())
+        {
+            var equipment = new Equipment { EquipmentID = 11, Name = "Test", Type = "Type", Specification = "Spec", Stock = 5 };
+            var result = _equipmentModel.UpdateEquipment(equipment);
+            result.Should().BeTrue();
+        }
     }
 
     [TestMethod]
     public void UpdateEquipment_ShouldReturnFalse_WhenExceptionOccurs()
     {
-        var equipment = new Equipment { EquipmentID = 1 };
-        var result = _equipmentModel.UpdateEquipment(equipment);
-        result.Should().BeFalse();
+        using (var scope = new TransactionScope())
+        {
+            var equipment = new Equipment { EquipmentID = -1 };
+            var result = _equipmentModel.UpdateEquipment(equipment);
+            result.Should().BeFalse();
+        }
     }
 
     [TestMethod]
     public void DeleteEquipment_ShouldReturnTrue_WhenDeleteSucceeds()
     {
-        var result = _equipmentModel.DeleteEquipment(1);
-        result.Should().BeTrue();
+        using (var scope = new TransactionScope())
+        {
+            // Reset the database to a known state
+            using (var connection = new SqlConnection(DatabaseHelper.GetConnectionString()))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(DatabaseHelper.GetResetProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            var result = _equipmentModel.DeleteEquipment(2);
+            result.Should().BeTrue();
+        }
     }
 
     [TestMethod]
     public void DoesEquipmentExist_ShouldReturnTrue_WhenEquipmentExists()
     {
-        var result = _equipmentModel.DoesEquipmentExist(5);
-        result.Should().BeTrue();
+        using (var scope = new TransactionScope())
+        {
+            var result = _equipmentModel.DoesEquipmentExist(5);
+            result.Should().BeTrue();
+        }
     }
 
     [TestMethod]
     public void GetEquipments_ShouldReturnEquipmentList_WhenDataExists()
     {
-        var result = _equipmentModel.GetEquipments();
-        result.Should().NotBeNull();
-        result.Count.Should().Be(12);
-        result[0].EquipmentID.Should().Be(1);
-        result[0].Name.Should().Be("Test1");
+        using (var scope = new TransactionScope())
+        {
+            using (var connection = new SqlConnection(DatabaseHelper.GetConnectionString()))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(DatabaseHelper.GetResetProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand("EXEC DeleteData", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand(DatabaseHelper.GetInsertDataProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand("EXEC InsertData @nrOfRows", connection))
+                {
+                    command.Parameters.AddWithValue("@nrOfRows", 10);
+                    command.ExecuteNonQuery();
+                }
+            }
+            var result = _equipmentModel.GetEquipments();
+            result.Should().NotBeNull();
+            result.Count.Should().Be(10);
+            result[0].EquipmentID.Should().Be(1);
+        }
     }
 }
--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9d424ec947ae31ce7e00fd3dc135aa95db19156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24207733c459e19e07fd55d39d68962a58ca15e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
