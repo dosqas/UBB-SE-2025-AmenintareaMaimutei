93fefe4ec39f1c00c09aa9e487dc565059cebaa9
Sampalean Sorin <sampaleansorin@outlook.com>
2025-04-10 19:50:34 +0300

    Added TestDrug and testDoctor

---
+++ Tests/Doctor/TestDoctorClassModel.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project.ClassModels;
+using FluentAssertions;
+using Project.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using Project.Utils;
+using System.Transactions;
+
+namespace Tests;
+
+[TestClass]
+public class TestDoctorClassModel
+{
+    private DoctorModel _doctorModel = new DoctorModel();
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _doctorModel = new DoctorModel();
+    }
+
+    [TestMethod]
+    public void AddDoctor_ShouldReturnTrue_WhenInsertSucceeds()
+    {
+        using (var scope = new TransactionScope())
+        {
+            var doctor = new Doctor
+            {
+                DoctorID = 0,
+                UserID = 1,
+                DepartmentID = 2,
+                Experience = 10,
+                Rating = 4.5f,
+                LicenseNumber = "ABC123"
+            };
+            var result = _doctorModel.AddDoctor(doctor);
+            result.Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    public void UpdateDoctor_ShouldReturnTrue_WhenUpdateSucceeds()
+    {
+        using (var scope = new TransactionScope())
+        {
+            var doctor = new Doctor
+            {
+                DoctorID = 1,
+                UserID = 1,
+                DepartmentID = 2,
+                Experience = 15,
+                Rating = 4.8f,
+                LicenseNumber = "XYZ789"
+            };
+            var result = _doctorModel.UpdateDoctor(doctor);
+            result.Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    public void UpdateDoctor_ShouldReturnFalse_WhenExceptionOccurs()
+    {
+        using (var scope = new TransactionScope())
+        {
+            var doctor = new Doctor { DoctorID = -1 };
+            var result = _doctorModel.UpdateDoctor(doctor);
+            result.Should().BeFalse();
+        }
+    }
+
+    [TestMethod]
+    public void DeleteDoctor_ShouldReturnTrue_WhenDeleteSucceeds()
+    {
+        using (var scope = new TransactionScope())
+        {
+            using (var connection = new SqlConnection(DatabaseHelper.GetConnectionString()))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(DatabaseHelper.GetResetProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+            var result = _doctorModel.DeleteDoctor(1);
+            result.Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    public void DoesDoctorExist_ShouldReturnTrue_WhenDoctorExists()
+    {
+        using (var scope = new TransactionScope())
+        {
+            var result = _doctorModel.DoesDoctorExist(1);
+            result.Should().BeTrue();
+        }
+    }
+
+    [TestMethod]
+    public void GetDoctors_ShouldReturnDoctorList_WhenDataExists()
+    {
+        using (var scope = new TransactionScope())
+        {
+            using (var connection = new SqlConnection(DatabaseHelper.GetConnectionString()))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(DatabaseHelper.GetResetProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand("EXEC DeleteData", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand(DatabaseHelper.GetInsertDataProcedureSql(), connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (var command = new SqlCommand("EXEC InsertData @nrOfRows", connection))
+                {
+                    command.Parameters.AddWithValue("@nrOfRows", 10);
+                    command.ExecuteNonQuery();
+                }
+            }
+            var result = _doctorModel.GetDoctors();
+            result.Should().NotBeNull();
+            result.Count.Should().Be(10);
+            result[0].DoctorID.Should().Be(1);
+        }
+    }
+}
--- Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
+++ Tests/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24207733c459e19e07fd55d39d68962a58ca15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41eac0abf600d892dd072cd234c0b4411a17f783")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
